03066523a05602a6e4c292dbeb711e99f4388cb5
Samuel Selleck <sellecksamuel@gmail.com>
2023-05-17 19:08:44 -0700

    set 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,11 @@ fn main() {
         696b65206120706f69736f6e6f7573206d757368726f6f6d";
     let res = hex_to_base64(test_str).unwrap();
     println!("{}", res);
+
+    let h1 = "1c0111001f010100061a024b53535009181c";
+    let h2 = "686974207468652062756c6c277320657965";
+    let res = hex_xor(h1, h2).unwrap();
+    println!("{}", res);
 }
 
 fn to_base_64_char(n: u8) -> char {
@@ -38,4 +43,45 @@ fn hex_to_base64(hex: &str) -> Result<String> {
         .flatten_ok();
 
     base64vals.map_ok(to_base_64_char).collect()
+}
+
+fn hex_to_byte_vec(hex: &str) -> Result<Vec<u8>> {
+    let mut vec = Vec::new();
+    let s_offset = hex.len() % 2;
+    if s_offset == 1 {
+        vec.push(u8::from_str_radix(&hex[..1], 16)?);
+    }
+
+    for i in (s_offset..hex.len()).step_by(2) {
+        vec.push(u8::from_str_radix(&hex[i..i + 2], 16)?);
+    }
+    Ok(vec)
+}
+
+fn byte_slice_to_hex(b: &[u8]) -> String {
+    b.iter()
+        .flat_map(|b| {
+            [
+                char::from_digit((b >> 4) as u32, 16).unwrap(),
+                char::from_digit((b & 0b00001111) as u32, 16).unwrap(),
+            ]
+        })
+        .collect()
+}
+
+fn byte_slice_xor(b1: &[u8], b2: &[u8]) -> Vec<u8> {
+    let n = b1.len();
+
+    let mut res = Vec::with_capacity(n);
+    for i in 0..n {
+        res.push(b1[i] ^ b2[i]);
+    }
+    res
+}
+
+fn hex_xor(h1: &str, h2: &str) -> Result<String> {
+    let b1 = hex_to_byte_vec(h1)?;
+    let b2 = hex_to_byte_vec(h2)?;
+    let b_res = byte_slice_xor(&b1, &b2);
+    Ok(byte_slice_to_hex(&b_res))
 }
