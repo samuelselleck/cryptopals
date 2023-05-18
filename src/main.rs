b69789202fef0f3e4e624be72173b32914910f49
Samuel Selleck <sellecksamuel@gmail.com>
2023-05-18 10:01:24 -0700

    set 1 part 3 (still needs cleanup)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![feature(iter_array_chunks)]
 
-use std::{error::Error, iter};
+use std::{collections::HashMap, error::Error, iter};
 
 use itertools::Itertools;
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
@@ -21,6 +21,7 @@ fn main() {
     let bs = hex_to_byte_vec(s).unwrap();
     let (guess, _) = brute_force_single_byte_xor_ciper(&bs);
     println!("{}", guess);
+    //brute_force_single_byte_xor_ciper_all(&bs);
 }
 
 fn brute_force_single_byte_xor_ciper(data: &[u8]) -> (String, u8) {
@@ -29,14 +30,62 @@ fn brute_force_single_byte_xor_ciper(data: &[u8]) -> (String, u8) {
             let decoded = single_byte_xor_decode(data, code);
             (decoded, code)
         })
-        .max_by_key(|(decoded, _)| textlike_score(decoded))
+        .min_by(|(d1, _), (d2, _)| {
+            let s1 = textlike_score(d1);
+            let s2 = textlike_score(d2);
+            s1.total_cmp(&s2)
+        })
         .unwrap();
     let s = String::from_utf8(best_decoded).unwrap_or("invalid utf8".into());
     (s, best_code)
 }
 
-fn textlike_score(decoded: &[u8]) -> u32 {
-    todo!()
+fn textlike_score(decoded: &[u8]) -> f64 {
+    let mut counts = HashMap::new();
+    for c in decoded {
+        *counts.entry(c.to_ascii_lowercase()).or_default() += 1;
+    }
+    const CHAR_FREQ: [(char, f64); 26] = [
+        ('a', 0.082),
+        ('b', 0.015),
+        ('c', 0.028),
+        ('d', 0.043),
+        ('e', 0.13),
+        ('f', 0.022),
+        ('g', 0.02),
+        ('h', 0.061),
+        ('i', 0.07),
+        ('j', 0.0015),
+        ('k', 0.0077),
+        ('l', 0.04),
+        ('m', 0.024),
+        ('n', 0.067),
+        ('o', 0.075),
+        ('p', 0.019),
+        ('q', 0.00095),
+        ('r', 0.06),
+        ('s', 0.063),
+        ('t', 0.091),
+        ('u', 0.028),
+        ('v', 0.0098),
+        ('w', 0.024),
+        ('x', 0.0015),
+        ('y', 0.02),
+        ('z', 0.00074),
+    ];
+
+    let total_chars: u32 = CHAR_FREQ
+        .into_iter()
+        .map(|(c, _)| counts.get(&(c as u8)).unwrap_or(&0))
+        .sum();
+
+    let mut score = 0.0; //lower is better
+    for (c, f) in CHAR_FREQ {
+        let count: u32 = counts.remove(&(c as u8)).unwrap_or_default();
+        let dist = (count as f64) / (total_chars as f64) - f;
+        score += dist.abs();
+    }
+    score
 }
 
 fn single_byte_xor_decode(data: &[u8], b: u8) -> Vec<u8> {
